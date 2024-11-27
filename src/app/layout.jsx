a05f492f217b1014e5335290f94165d2ab04d880
Jake Wall <walljacobc@gmail.com>
2024-11-27 14:19:00 -0500

    initial push

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,12 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; // Import the Inter font
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
-  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({
+    children,
+}) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
